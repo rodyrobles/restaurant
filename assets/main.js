e7828c1e3edfce51124d6d3c5ca8ded324278001
rodyrobles <rodyrob@gmail.com>
2017-06-05 23:02:50 -0500

    toggle div classes using jquery

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,9 @@
-var paragraphs = document.getElementsByTagName("p");
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+    });
+
+/*var paragraphs = document.getElementsByTagName("p");
 for (var i = 0; i < paragraphs.length; i++) {
 var paragraph = paragraphs.item(i);
 paragraph.style.setProperty("color", "yellow", null);
-}
+}*/
